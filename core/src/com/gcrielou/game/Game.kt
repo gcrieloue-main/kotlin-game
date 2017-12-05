61437b18e16fe4b69b0fc1458d6cea869cc9196b
hellsdark <hellsdark@gmail.com>
2017-12-05 14:02:19 +0100

    Character move improvements

--- core/src/com/gcrielou/game/Game.kt
+++ core/src/com/gcrielou/game/Game.kt
@@ -2,6 +2,7 @@ package com.gcrielou.game
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
@@ -54,12 +55,22 @@ class Game : ApplicationAdapter() {
         private var lastAnimationDrawing: Float = 0f
         private var currentSprite = 1
         private var speed = 0.1
-        private var currentState = "RUNNING"
+        var currentState = "IDLE"
         private var lastState = "RUNNING"
 
-        var statesSprites: Map<String, Array<Sprite>> = mapOf("RUNNING" to arrayOf(Sprite(9, 1), Sprite(9, 2), Sprite(9, 3)))
+        var statesSprites: Map<String, Array<Sprite>> = mapOf(
+                "RUNNING" to arrayOf(Sprite(9, 1), Sprite(9, 2), Sprite(9, 3)),
+                "IDLE" to arrayOf(Sprite(7, 1), Sprite(7, 2), Sprite(7, 3), Sprite(7, 3)),
+                "JUMP" to arrayOf(Sprite(12, 1), Sprite(12, 2))
+        )
 
-        public fun drawCharacter(batch: Batch, img: Texture) {
+        fun drawCharacter(batch: Batch, img: Texture) {
+
+            if (currentState != lastState) {
+                lastAnimationDrawing = 0f
+                currentSprite = 0
+                lastState = currentState
+            }
 
             var sprites: Array<Sprite>? = statesSprites.get(currentState)
 
@@ -67,18 +78,25 @@ class Game : ApplicationAdapter() {
             lastAnimationDrawing += deltaTime
 
             if (lastAnimationDrawing >= speed) {
-                currentSprite = currentSprite % sprites!!.size + 1
+                currentSprite = (currentSprite + 1) % sprites!!.size
                 lastAnimationDrawing = 0f
             }
 
             batch.drawSprite(img, 200f, 50f,
-                    SPRITE_SIZE, 9, currentSprite)
+                    SPRITE_SIZE, sprites!![currentSprite].x, sprites!![currentSprite].y)
         }
     }
 
     var character: Character = Character()
 
     private fun draw() {
+
+        when {
+            Gdx.input.isKeyPressed(Input.Keys.RIGHT) -> character.currentState = "RUNNING"
+            Gdx.input.isKeyPressed(Input.Keys.DOWN) -> character.currentState = "IDLE"
+            Gdx.input.isKeyPressed(Input.Keys.UP) -> character.currentState = "JUMP"
+        }
+
         character.drawCharacter(batch, img)
     }
 
